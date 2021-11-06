bf11fbaeced04d75c1c72218a125520fc58e938e
aaaaaaaaaaaaaaaa <2002bogdan@gmail.com>
2021-11-06 13:01:12 +0200

    Add saves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,10 +13,22 @@ public class GameManager : MonoBehaviour
         UICanvas = GameObject.FindGameObjectWithTag("UICanvas").GetComponent<Canvas>();
         Cache = GetComponent<CacheScript>();
         PlayerData = GetComponent<PlayerData>();
+        PlayerData.LoadData();
     }
 
     public BaseWindow InstantiateWindow(string windowName)
     {
         return Instantiate(Cache.GetWindowByName(windowName), Consts.DEFAULT_WINDOW_SPAWN_POSITION, Quaternion.identity, UICanvas.transform);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+            PlayerData.SaveData();
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerData.SaveData();
     }
 }
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour
@@ -8,6 +10,7 @@ public class PlayerData : MonoBehaviour
     [HideInInspector] public int SabotageProcent;
     [HideInInspector] public int CompletedMissionsCount;
     [HideInInspector] public int CurrentScore;
+    [HideInInspector] public int MaxScore;
 
     [HideInInspector] public List<Mission> CurrentMissions;
 
@@ -25,6 +28,7 @@ public class PlayerData : MonoBehaviour
         SabotageProcent = GetStartSabotageProcentByLevel(LevelNumber);
         CompletedMissionsCount = 0;
         CurrentScore = 0;
+        MaxScore = 0;
     }
 
     private int GetStartSabotageProcentByLevel(int levelNumber)
@@ -32,4 +36,53 @@ public class PlayerData : MonoBehaviour
         //TODO: create sabotage proc balance
         return 50;
     }
+
+        #region SaveLoad
+    private SaveData CreateSaveGameObject()
+    {
+        SaveData savedData = new SaveData();
+        savedData.MaxScore = MaxScore;
+
+        return savedData;
+    }
+
+    public void SaveData()
+    {
+        SaveData savedData = CreateSaveGameObject();
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, savedData);
+        file.Close();
+
+        Debug.Log("Game Saved");
+    }
+
+    public void LoadData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+            SaveData savedData = (SaveData)bf.Deserialize(file);
+            file.Close();
+
+            MaxScore = savedData.MaxScore;
+
+            Debug.Log("Game Loaded");
+        }
+        else
+        {
+            MaxScore = 0;
+
+            Debug.Log("No game saved!");
+        }
+    }
+    #endregion
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public int MaxScore;
 }
