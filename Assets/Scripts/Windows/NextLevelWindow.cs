fdc7f3248975342e37419fa8070b8849795ffa22
aaaaaaaaaaaaaaaa <2002bogdan@gmail.com>
2021-11-07 14:25:04 +0200

    Add next level window

---
+++ Assets/Scripts/Windows/NextLevelWindow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NextLevelWindow : BaseWindow
+{
+    [SerializeField] private TextMeshProUGUI locationNameText;
+    [SerializeField] private TextMeshProUGUI debafText;
+    [SerializeField] private TextMeshProUGUI debafDescriptionText;
+}
